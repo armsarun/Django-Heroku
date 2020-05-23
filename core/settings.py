3d0b801ccc5cb77c34ad8918b95f7cdf36bf4fdb
arunkumar <arunkumarp93@yahoo.com>
2020-05-24 00:16:58 +0530

    Procfile added

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 try:
    DEBUG = config('DEBUG')
 except UndefinedValueError:
-    DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
